f800481326a62e3b9dca19ee23b211a44be9e162
Sangeetha11o1 <71844465+Sangeetha11o1@users.noreply.github.com>
2023-05-31 16:43:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './Footer';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Footer/>
+      
+      {/* Your main content goes here */}
+      
+    </div>
+  );
+};
+
+export default App;
